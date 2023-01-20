9d65a8f31ade86d82ea5c586d87b570d0fb29612
Ирина <mi.irene97@gmail.com>
2023-01-20 21:00:24 +0300

    Задачи 2.1 - 2.3 готовы

---
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -0,0 +1,10 @@
+package pro.sky.telegrambot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.sky.telegrambot.model.NotificationTask;
+
+public interface NotificationTaskRepository extends
+        JpaRepository<NotificationTask, Long> {
+
+
+}
