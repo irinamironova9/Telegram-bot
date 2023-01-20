fcafc46d92611344f4febbb8d49d8ca8eca84591
Ирина <mi.irene97@gmail.com>
2023-01-20 19:24:47 +0300

    Задача 1 готова

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,9 +30,18 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message().text().equals("/start")) {
+                logger.info("Sending the greeting message");
+                long chatId = update.message().chat().id();
+                SendMessage message = new SendMessage(chatId,
+                        "Hi! I`m Bob the bot");
+                SendResponse response = telegramBot.execute(message);
+                if (!response.isOk()) {
+                    logger.error("Could not send the greeting message! " +
+                            "Error code: {}", response.errorCode());
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
-
 }
